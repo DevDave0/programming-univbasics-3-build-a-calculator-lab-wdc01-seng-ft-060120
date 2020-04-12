75d6d8ec1ea68581809e25ca4b89451c10bab5c4
David Chung <dbchung3@yahoo.com>
2020-04-12 02:17:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,7 +3,7 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  num2 - num1 
+  num1 - num2 
 
 end
 
@@ -13,10 +13,12 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 
 end
 
 def modulo(num1, num2)
+  num1 % num2
 
 end
 
